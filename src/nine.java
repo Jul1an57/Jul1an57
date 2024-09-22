1c5b050af04813281eff8a18ad946725312967d6
Jul1an57 <2013672653@qq.com>
2024-09-22 17:48:24 +0800

    Update nine.java

--- src/nine.java
+++ src/nine.java
@@ -1,11 +1,11 @@
 public class nine {
-    public static void main(String[] args) {
-        double width = 4.5;
-        double height = 7.9;
+    public static void main(String[] args) {  
+        
+        double width = 4.5;  
+        double height = 7.9;  
+        double area = width * height;  
+        double perimeter = 2 * (width + height);  
+        System.out.println("The area of the rectangle is: " + area);  
+        System.out.println("The perimeter of the rectangle is: " + perimeter);  
+    }  
 
-        double perimeter = (width + height)*2;
-        System.out.println(perimeter);
-        double area = 4.5 * 7.9;
-        System.out.println(area);
-    }
-}
