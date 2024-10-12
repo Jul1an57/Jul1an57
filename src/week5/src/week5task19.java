61ffdd2f8af1a847187eb673301024c7a47dc705
Jul1an57 <2013672653@qq.com>
2024-10-13 01:16:03 +0800

    Add files via upload

---
+++ src/week5/src/week5task19.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class week5task19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int count = input.nextInt();
+
+        System.out.print("Enter an amount: ");
+        double amount = input.nextDouble();
+
+        System.out.printf("Count: %,d\n", count);
+        System.out.printf("Amount: $%,.2f\n", amount);
+
+        input.close();
+    }
+}
+
